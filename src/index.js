8888124d2f5e65333e061055abf11345346590b5
NEWHELL69 <kushagra0304@gmail.com>
2023-01-01 22:15:11 +0530

    Revert "Client side routing complete"
    This reverts commit dac2e02a433a3e7910d54c360897c697b1bac981.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-const Intro = lazy(() => import('./routes/intro'));
-const ErrorPage = lazy(() => import('./error-page'));
-const Login = lazy(() => import('./routes/login'));
-const Home = lazy(() => import('./routes/home'));
-const CreateAccount = lazy(() => import('./routes/createAccount'));
-const Search = lazy(() => import('./routes/search'));
-const Sell = lazy(() => import('./routes/sell'));
-const Chat = lazy(() => import('./routes/chat'));
-const Account = lazy(() => import('./routes/account'));
-
-const App = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/" element={<Intro />} />
-        <Route path="login" element={<Login />} />
-        <Route path="home" element={<Home />} />
-        <Route path="createAccount" element={<CreateAccount />} />
-        <Route path="search" element={<Search />} />
-        <Route path="sell" element={<Sell />} />
-        <Route path="chat" element={<Chat />} />
-        <Route path="account" element={<Account />} />
-      </Routes>
-    </Suspense>
-  </Router>
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
